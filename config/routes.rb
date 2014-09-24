c2f5fe58affdfbbcb9c1c89ccce9c5ef1bd5752f
rasbabbo <rasbabbo@yahoo.com>
2014-09-23 20:26:31 -0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 	resources :graft_templates
 
 	get "legislators/:state_id", to: "grafts#find_legislators"
-
+	get "legislator/:cid", to: "grafts#find_legislator"
+	get "donors/:cid", to: "grafts#find_donors"
+	get "industries/:cid", to: "grafts#find_industries"
 
   # match "*path", to: "grafts#index", via: "get"
   
